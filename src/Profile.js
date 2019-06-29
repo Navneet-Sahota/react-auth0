d340d7e63f5749ee087388bf773c40ce993a7465
Navneet Sahota <navneet.sahota@outlook.com>
2019-06-29 18:44:13 +0530

    logout, signup & user profile

--- src/Profile.js
+++ src/Profile.js
@@ -1,8 +1,41 @@
 import React, { Component } from "react";
 
 class componentName extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			profile: null,
+			err: "",
+		};
+	}
+
+	componentDidMount() {
+		this.loadUserProfile();
+	}
+
+	loadUserProfile = () => {
+		this.props.auth.getProfile((profile, err) =>
+			this.setState({ profile, err })
+		);
+	};
+
 	render() {
-		return <h1>Profile</h1>;
+		const { profile } = this.state;
+		if (!profile) {
+			return null;
+		}
+		return (
+			<>
+				<h1>Profile</h1>
+				<p>{profile.nickname}</p>
+				<img
+					style={{ maxHeight: "50px", maxWidth: "50px" }}
+					src={profile.pic}
+					alt="profile pic"
+				/>
+				<pre>{JSON.stringify(profile, null, 2)}</pre>
+			</>
+		);
 	}
 }
 
