164952aa9680290e6569abdaa157577fc3f66906
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-01 02:54:47 +0530

    remove duplicate login button

--- src/Home.js
+++ src/Home.js
@@ -3,15 +3,11 @@ import { Link } from "react-router-dom";
 
 class Home extends Component {
 	render() {
-		const { isAuthenticated, login } = this.props.auth;
+		const { isAuthenticated } = this.props.auth;
 		return (
 			<div>
 				<h1>Home</h1>
-				{isAuthenticated() ? (
-					<Link to="/profile">View Profile</Link>
-				) : (
-					<button onClick={login}>Log In</button>
-				)}
+				{isAuthenticated() ? <Link to="/profile">View Profile</Link> : null}
 			</div>
 		);
 	}
